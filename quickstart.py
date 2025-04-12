46e6423dd46c909987bd387999ce34c3014c1e97
Artem Kiselev <kiselevart1403@gmail.com>
2025-04-12 21:29:34 +0700

    feat: scans for pdfs

--- quickstart.py
+++ quickstart.py
@@ -6,20 +6,12 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-# If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/drive.metadata.readonly"]
 
 def main():
-  """Shows basic usage of the Drive v3 API.
-  Prints the names and ids of the first 10 files the user has access to.
-  """
   creds = None
-  # The file token.json stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first
-  # time.
   if os.path.exists("token.json"):
     creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-  # If there are no (valid) credentials available, let the user log in.
   if not creds or not creds.valid:
     if creds and creds.expired and creds.refresh_token:
       creds.refresh(Request())
@@ -28,19 +20,29 @@ def main():
           "credentials.json", SCOPES
       )
       creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
     with open("token.json", "w") as token:
       token.write(creds.to_json())
 
   try:
     service = build("drive", "v3", credentials=creds)
 
-    # Call the Drive v3 API
+    query = (
+        "mimeType='application/pdf' or "
+        "mimeType='application/epub+zip' or "
+        "mimeType='application/x-djvu' or "
+        "mimeType='application/x-mobipocket-ebook'"
+    )
+
     results = (
         service.files()
-        .list(pageSize=10, fields="nextPageToken, files(id, name)")
+        .list(
+            q=query,
+            pageSize=10,
+            fields="nextPageToken, files(id, name, mimeType)"
+        )
         .execute()
     )
+
     items = results.get("files", [])
 
     if not items:
@@ -50,9 +52,7 @@ def main():
     for item in items:
       print(f"{item['name']} ({item['id']})")
   except HttpError as error:
-    # TODO(developer) - Handle errors from drive API.
     print(f"An error occurred: {error}")
-
 
 if __name__ == "__main__":
   main()
